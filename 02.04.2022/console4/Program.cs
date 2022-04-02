fd35324ccc9dd86fc93b0bfca1018bd22e9ad0f7
theHainz <58784402+theHainz@users.noreply.github.com>
2022-04-02 09:55:25 +0200

    C# 02.04.2022 09:54

---
+++ 02.04.2022/console4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace console4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = ("C:\\Users\\frygus.S309.000\\Desktop\\C#\\02.04.2022\\filedir\\file.txt");
+
+            string Name;
+            string Surname;
+            int Phonenumber;
+            string Phonenumbertostring;
+          
+
+            Console.WriteLine("What's your name?");
+            Name = Console.ReadLine();
+
+            Console.WriteLine("What's your surname?");
+            Surname = Console.ReadLine();
+
+            Console.WriteLine("What's your phonenumber?");
+            Phonenumbertostring = Console.ReadLine();
+
+            Phonenumber = Convert.ToInt32(Phonenumbertostring);
+
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(Name);
+                    sw.WriteLine(Surname);
+                    sw.WriteLine(Phonenumber);
+                }
+            }
+        }
+    }
+}
