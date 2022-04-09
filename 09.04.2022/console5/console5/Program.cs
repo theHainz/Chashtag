8412cac7179400e8ed95aa9f6c425be8094af160
theHainz <58784402+theHainz@users.noreply.github.com>
2022-04-09 11:04:02 +0200

    09.04.2022 10:02

---
+++ 09.04.2022/console5/console5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace console5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //el
+            string[] givenNames = new string[3];
+            List<string> givenNamesList = new List<string>();
+            int limit = 10000;
+            int counter = 0;
+
+            while(counter < limit)
+            {
+                Console.WriteLine("Current List length");
+                Console.WriteLine(givenNamesList.Count);
+                Console.WriteLine("Gib Name");
+                string Name = Console.ReadLine();
+                givenNamesList.Add(Name);
+                //currentindex++
+                Console.Clear();
+            }
+        }
+
+        static string[] AddtoTable(string[] table, string newElement)
+        {
+            string[] NewTable = new string[table.Length + 1];
+            for (int i = 0; i < table.Length; i++)
+            {
+                NewTable[i] = table[i];
+            }
+
+            NewTable[NewTable.Length - 1] = newElement;
+            return NewTable;
+            
+        }
+    }
+}
---
+++ 09.04.2022/console6/console6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace console6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human = new Human();
+
+            human.Haircolour = "blond";
+            human.Eye = "blue";
+            human.Age = 16;
+
+            Human human2 = new Human();
+            human2.Haircolour = "dark";
+            human2.Eye = "brown";
+            human2.Age = 25;
+            
+            Console.WriteLine("Human");
+            Console.WriteLine(human.Haircolour);
+            Console.WriteLine(human.Eye);
+            Console.WriteLine(human.Age);
+            Console.WriteLine("");
+            Console.WriteLine("Human2");
+            Console.WriteLine(human2.Haircolour);
+            Console.WriteLine(human2.Eye);
+            Console.WriteLine(human2.Age);
+        }
+
+        class Human
+        {
+            public string Haircolour;
+            public int Age;
+            public string Eye;
+            
+        }
+    }
+}
