55e63e8d0176d2241aea752f02c4ba46dda094d7
theHainz <58784402+theHainz@users.noreply.github.com>
2022-04-09 08:24:46 +0200

    New file upload that should work with gitignore

---
+++ 02.04.2022/console4/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+
+
+namespace console4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = ("");
+
+            Console.WriteLine("What do you want to do with the file?");
+            Console.WriteLine("You can 'writeinfile', ");
+            string answer;
+            answer = Console.ReadLine();
+
+            switch (answer)
+            {
+                case "writeinfile":
+                    writeinfile(path);
+                    break;
+
+            }
+        }
+
+        static void writeinfile(string path) {
+
+            string name;
+            string surname;
+            int phonenumber;
+            string phonenumbertostring;
+
+            //asking the user for their information (and sending it to the chinese)
+            Console.WriteLine("What's your name?");
+            name = Console.ReadLine();
+
+            Console.WriteLine("What's your surname?");
+            surname = Console.ReadLine();
+
+            Console.WriteLine("What's your phonenumber?");
+            phonenumbertostring = Console.ReadLine();
+
+            phonenumber = Convert.ToInt32(phonenumbertostring);
+                
+            // count the line number.
+            int counter = 0;
+
+            StreamReader streamReader = new StreamReader(path);
+            while (streamReader.ReadLine() != null)
+            {
+                counter++;
+            }
+
+            streamReader.Close();
+
+            //bad implementation but I'm too tired to do something more creative
+            if (!File.Exists(path))
+            {
+
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine($"{counter} {name} {surname} {phonenumber}");
+                }
+            }
+            else if (File.Exists(path))
+            {
+                File.Delete(path);
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine($"{counter} {name} {surname} {phonenumber}");
+                }
+            }
+        }
+    }
+}
